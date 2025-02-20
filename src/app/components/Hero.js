ab8cb839c803bf44d4b0b0a428b2b2ce8f7bffdd
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-02-20 18:22:39 +0200

    Add current implementation of Hero component

---
+++ src/app/components/Hero.js
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Typed from "typed.js";
+
+const Hero = () => {
+  const el = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Web Developer",
+        "Basketball Enthusiast",
+        "DevOps Engineer",
+        "Learner",
+      ],
+      typeSpeed: 45,
+      backSpeed: 60,
+      loop: false,
+      showCursor: false,
+      contentType: "html",
+    };
+
+    const typed = new Typed(el.current, options);
+    return () => {
+      typed.destroy();
+    };
+  });
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen text-center text-white p-8">
+      <h1 className="text-5xl sm:text-6xl font-bold">
+        Hi, I am Samson Lukhele.
+      </h1>
+      <p className="text-xl sm:text-2xl mt-4">
+        I'm a{" "}
+        <span className="font-mono text-light-blue rounded-md">
+          ["
+          <span ref={el} />
+          "]
+        </span>
+        .
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
