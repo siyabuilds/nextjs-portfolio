1ff727fcbb2a5cd3d9dbcf8cb43cb0fd5c67f380
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-02-25 17:26:09 +0200

    Properly resize and make Hero responsive

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -21,17 +21,18 @@ const Hero = () => {
     return () => {
       typed.destroy();
     };
-  });
+  }, []);
+
   return (
     <section className="flex flex-col md:flex-row items-center justify-center min-h-screen text-center text-white p-8">
-      <div className="md:w-1/2 mb-4 md:mb-0">
+      <div className="md:w-1/3 mb-4 md:mb-0">
         <img
           src="https://github.com/markuptitan.png"
           alt="Avatar"
           className="rounded-full w-64 h-64 md:w-48 md:h-48 mx-auto"
         />
       </div>
-      <div className="md:w-1/2 mt-4 md:mt-0">
+      <div className="md:w-2/3 mt-4 md:mt-0">
         <h1 className="text-6xl sm:text-4xl font-bold">
           Hi, I am Samson Lukhele.
         </h1>
