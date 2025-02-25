171b5f620b247c26ca2e667a5dfb9496dd0e8b6b
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-02-25 18:25:58 +0200

    Update metadata for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,11 +26,32 @@ export const metadata = {
   title: "Samson Lukhele's Portfolio",
   description:
     "This is a Next.js Web Dev portfolio showcasing Samson Lukhele's projects",
+  keywords: "web development, portfolio, Samson Lukhele, Next.js, React",
+  openGraph: {
+    title: "Samson Lukhele's Portfolio",
+    description:
+      "This is a Next.js Web Dev portfolio showcasing Samson Lukhele's projects",
+    url: "https://nextjs.markuptitan.site",
+    image: "https://github.com/markuptitan.png",
+    site_name: "Samson Lukhele's Portfolio",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:image" content={metadata.openGraph.image} />
+        <meta property="og:site_name" content={metadata.openGraph.site_name} />
+      </head>
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased bg-dark-gray text-white`}
       >
