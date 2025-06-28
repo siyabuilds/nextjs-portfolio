da58baabfc0bf631d5a92a7d4d6be5decb12da17
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-06-28 19:13:26 +0200

    delete everything and prepare for a teardown

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import Hero from "@/app/components/Hero";
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <Hero />
-    </main>
-  );
-}
